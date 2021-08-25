b822611cdefdc85bcd6410c2fdc38c043b8796c7
Onome Okuma <okumaonome@gmail.com>
2021-08-25 13:45:13 +0100

    Added commands to build and deploy services

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-echo "this is me testing"
+
+echo "Building Publisher Server"
+cd ./Publisher
+mvn package -DskipTests
+
+echo "Starting Publisher Server on port 5000"
+java -jar ./target/publisher-0.0.1.jar
+echo "Publisher Server started on port 5000"
+
+cd ..
+
+echo "Building Subscriber Server"
+cd ./Subscriber
+mvn package -DskipTests
+
+echo "Starting Subscriber Server on port 9000"
+java -jar ./target/subscriber-0.0.1.jar
+echo "Subscriber Server started on port 9000"
+
+echo "Services Started"
+
+
