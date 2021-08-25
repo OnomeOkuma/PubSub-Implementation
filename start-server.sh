6636531db32e482db652881d41bedb50d27ff98f
Onome Okuma <okumaonome@gmail.com>
2021-08-25 14:00:04 +0100

    Fixed command execution order to prevent handups

--- start-server.sh
+++ start-server.sh
@@ -4,20 +4,14 @@ echo "Building Publisher Server"
 cd ./publisher
 mvn package -DskipTests
 
-echo "Starting Publisher Server on port 5000"
-java -jar ./target/publisher-0.0.1.jar
-echo "Publisher Server started on port 5000"
-
 cd ..
 
 echo "Building Subscriber Server"
-cd ./pubscriber
+cd ./subscriber
 mvn package -DskipTests
 
-echo "Starting Subscriber Server on port 9000"
-java -jar ./target/subscriber-0.0.1.jar
-echo "Subscriber Server started on port 9000"
-
+echo "Starting Services"
+java -jar ./target/publisher-0.0.1.jar & java -jar ./target/subscriber-0.0.1.jar
 echo "Services Started"
 
 
