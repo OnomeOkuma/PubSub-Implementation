929d33b84d70a39b389259476b41210dfe2de378
Onome Okuma <okumaonome@gmail.com>
2021-08-25 14:05:50 +0100

    Fix to correct jar path

--- start-server.sh
+++ start-server.sh
@@ -11,7 +11,12 @@ cd ./subscriber
 mvn package -DskipTests
 
 echo "Starting Services"
-java -jar ./target/publisher-0.0.1.jar & java -jar ./target/subscriber-0.0.1.jar
+
+& java -jar ./target/subscriber-0.0.1.jar
+cd .. & cd ./publisher
+& java -jar ./target/publisher-0.0.1.jar 
+
+
 echo "Services Started"
 
 
